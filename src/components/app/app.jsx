eac8b67b926549fcb4e8bba1b08ff0d28c3c7ff6
Amir Ashizhev <amirashizhev@yandex.ru>
2023-06-01 21:05:17 +0300

    feat: profile-block without fonts and props

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,7 +1,8 @@
+import ProfileBlock from '../profile-block/profile-block';
 import styles from './app.module.scss';
 
 function App() {
-	return <div className={styles.app}>hello</div>;
+	return <div className={styles.app}>hello<ProfileBlock/></div>;
 }
 
 export default App;
