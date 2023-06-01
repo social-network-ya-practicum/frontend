0c352c8473f6df85d686d3c7b07d67794e365c16
Amir Ashizhev <amirashizhev@yandex.ru>
2023-06-01 21:15:20 +0300

    fix: profile-block поправил верстку в соответствии с макетом

--- src/components/profile-block/profile-block.jsx
+++ src/components/profile-block/profile-block.jsx
@@ -5,22 +5,11 @@ function ProfileBlock() {
     <div className={styles.block}>
       <div className={styles.photo}> </div>
       <h2 className={styles.name}>Юлия Левакова</h2>
-      <p className={styles.prof}>Бухгалтер</p>
-
-      <ul className={styles.list}>
-        <li className={styles.item}>
-          <p className={styles.text}>Публикации</p>
-          <span className={styles.span}>8</span>
-        </li>
-        <li className={styles.item}>
-          <p className={styles.text}>Группы</p>
-          <span className={styles.span}>3</span>
-        </li>
-        <li className={styles.item}>
-          <p className={styles.text}>Репосты</p>
-          <span className={styles.span}>3</span>
-        </li>
-      </ul>
+      <p className={styles.role}>Админ</p>
+      <div className={styles.item}>
+        <p className={styles.text}>Публикации</p>
+        <span className={styles.span}>8</span>
+      </div>
     </div>
   )
 }
