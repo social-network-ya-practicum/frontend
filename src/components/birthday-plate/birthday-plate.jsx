56955cccb0ff9e93d0a6e5d8f82f832512755241
Stas Razborskii <stasaktivniy@yandex.ru>
2023-06-03 14:44:01 +0300

    feat: добавлен компонент "список дней рождений"

---
+++ src/components/birthday-plate/birthday-plate.jsx
@@ -0,0 +1,67 @@
+import PropTypes from 'prop-types';
+import iconCake from './icon-birthday-cake.svg';
+import styles from './birthday-plate.module.scss';
+
+function BirthdayPlate({ data }) {
+	return (
+		<section className={styles.birthdayPlate}>
+			<div className={styles.birthdayPlate__header}>
+				<h2 className={styles.birthdayPlate__title}>Дни рождения</h2>
+			</div>
+			<section className={styles.birthdayPlate__list}>
+				{data.length > 0 ? (
+					data.slice(0, 3).map((person) => (
+						<article key={person.id} className={styles.birthdayPlate__item}>
+							<div className={styles.birthdayPlate__imgWrapper}>
+								<img src={person.photo} alt="Аватар пользователя" />
+							</div>
+							<div className={styles.birthdayPlate__info}>
+								<p>
+									{person.first_name} {person.last_name}
+								</p>
+								<p>{person.birthday_date}</p>
+							</div>
+						</article>
+					))
+				) : (
+					<article className={styles.birthdayPlate__birthdayFreeDay}>
+						<img src={iconCake} alt="cake" />
+						<p>
+							Сегодня в нашей компании никто не отмечает День рождения, но мы
+							желаем вам хорошего дня!
+						</p>
+					</article>
+				)}
+			</section>
+		</section>
+	);
+}
+
+export default BirthdayPlate;
+
+BirthdayPlate.propTypes = {
+	data: PropTypes.arrayOf(
+		PropTypes.shape({
+			id: PropTypes.number.isRequired,
+			first_name: PropTypes.string.isRequired,
+			last_name: PropTypes.string.isRequired,
+			birthday_date: PropTypes.string.isRequired,
+		})
+	).isRequired,
+};
+
+BirthdayPlate.propTypes = {
+	data: PropTypes.arrayOf(
+		PropTypes.shape({
+			first_name: PropTypes.string,
+		})
+	),
+};
+
+BirthdayPlate.getDefaultProps = {
+	data: PropTypes.arrayOf(
+		PropTypes.shape({
+			photo: '',
+		})
+	),
+};
---
+++ src/components/birthday-plate/birthday-plate.stories.js
@@ -0,0 +1,65 @@
+import BirthdayPlate from './birthday-plate';
+
+export default {
+	title: 'Birthday Plate',
+	component: BirthdayPlate,
+	tags: ['autodocs'],
+};
+
+export const ExampleBirthdayFree = {
+	args: {
+		data: [],
+	},
+};
+
+export const ExampleOneBirthday = {
+	args: {
+		data: [
+			{
+				id: 0,
+				photo: null,
+				first_name: 'Helen11111111111111111111111111',
+				last_name: 'Montana',
+				birthday_date: '10 July',
+			},
+		],
+	},
+};
+
+export const ExampleFourBirthday = {
+	args: {
+		data: [
+			{
+				id: 0,
+				photo: null,
+				first_name: 'Helen11111111111111111111111111',
+				last_name: 'Montana11111111111111111111111111',
+				birthday_date: '10 July',
+			},
+			{
+				id: 1,
+				photo:
+					'https://avatars.mds.yandex.net/i?id=d09d19224c896df74b406968dcf677afdda39416-9151370-images-thumbs&n=13',
+				first_name: 'Lora',
+				last_name: 'Montana',
+				birthday_date: '11 Desember',
+			},
+			{
+				id: 2,
+				photo:
+					'https://avatars.mds.yandex.net/i?id=d09d19224c896df74b406968dcf677afdda39416-9151370-images-thumbs&n=13',
+				first_name: 'Any',
+				last_name: 'Montana',
+				birthday_date: '1 Feb',
+			},
+			{
+				id: 3,
+				photo:
+					'https://avatars.mds.yandex.net/i?id=d09d19224c896df74b406968dcf677afdda39416-9151370-images-thumbs&n=13',
+				first_name: 'Mia',
+				last_name: 'Montana',
+				birthday_date: '12 Oct',
+			},
+		],
+	},
+};
