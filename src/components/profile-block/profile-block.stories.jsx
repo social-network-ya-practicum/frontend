e3b0ffb6373caf04e8f039f3a858b6b7273cda55
Aliaksandr Lahunou <mymail15-87y@yandex.ru>
2023-06-04 17:02:05 +0300

    chore: исправил файл editorconfig (2 space вместо 2 tab)

--- src/components/profile-block/profile-block.stories.jsx
+++ src/components/profile-block/profile-block.stories.jsx
@@ -1,29 +1,28 @@
 import ProfileBlock from './profile-block';
 import '../../index.scss';
 
-
 export default {
-  title: 'Profile Block',
-  component: ProfileBlock,
-  tags: ['autodocs'],
-}
+	title: 'Profile Block',
+	component: ProfileBlock,
+	tags: ['autodocs'],
+};
 
 export const AdminExample = {
-  args: {
-    role: 'admin',
-    info: {
-      firstName: 'Андрей',
-      lastName: 'Иванов',
-    },
-  },
+	args: {
+		role: 'admin',
+		info: {
+			firstName: 'Андрей',
+			lastName: 'Иванов',
+		},
+	},
 };
 
 export const UserExample = {
-  args: {
-    role: 'user',
-    info: {
-      firstName: 'Юлия',
-      lastName: 'Леденцова',
-    },
-  },
+	args: {
+		role: 'user',
+		info: {
+			firstName: 'Юлия',
+			lastName: 'Леденцова',
+		},
+	},
 };
