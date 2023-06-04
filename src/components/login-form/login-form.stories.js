0a621c9777b37809fead6764981f43fa5ac4c901
Aliaksandr Lahunou <mymail15-87y@yandex.ru>
2023-06-04 21:51:42 +0300

    feat: добавил компонент LoginForm

---
+++ src/components/login-form/login-form.stories.js
@@ -0,0 +1,14 @@
+import LoginForm from './login-form';
+import '../../index.scss';
+
+export default {
+	title: 'LoginForm',
+	component: LoginForm,
+	tags: ['autodocs'],
+};
+
+export const Default = {
+	args: {
+		onSubmit: () => undefined,
+	},
+};
