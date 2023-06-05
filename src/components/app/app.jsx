652eb50b0c66897371f7ae34c939583b0564fed7
Aliaksandr Lahunou <mymail15-87y@yandex.ru>
2023-06-06 00:01:28 +0300

    chore: усноновил react-router && добавил роуты в app

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,7 +1,32 @@
-import styles from './app.module.scss';
+import { Outlet, Route, Routes } from 'react-router-dom';
 
 function App() {
-	return <div className={styles.app}>hello</div>;
+	return (
+		<Routes>
+			<Route
+				element={
+					<>
+						<div>Layout</div>
+						<Outlet />
+					</>
+				}
+			>
+				<Route path="/" element={<Outlet />}>
+					<Route index element={<div>Route /</div>} />
+					<Route path="contacts" element={<div>Route /contacts</div>} />
+					<Route
+						path="contacts/:contactId"
+						element={<div>Route /contacts/:contactId</div>}
+					/>
+					<Route path="user" element={<div>Route /user</div>} />
+					<Route path="user/edit" element={<div>Route /user/edit</div>} />
+				</Route>
+				<Route path="/login" element={<div>Route /login</div>} />
+				<Route path="/register" element={<div>Route /register</div>} />
+			</Route>
+			<Route path="*" element={<div>Page 404</div>} />
+		</Routes>
+	);
 }
 
 export default App;
