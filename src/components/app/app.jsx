71ee3dd0fbe10230b8aa58e69da36ee599e7ccd9
Aliaksandr Lahunou <mymail15-87y@yandex.ru>
2023-06-06 00:54:48 +0300

    feat: добавил компонент main-layout

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,28 +1,22 @@
 import { Outlet, Route, Routes } from 'react-router-dom';
+import MainLayout from '../layouts/main-layout/main-layout';
 
 function App() {
 	return (
 		<Routes>
-			<Route
-				element={
-					<>
-						<div>Layout</div>
-						<Outlet />
-					</>
-				}
-			>
+			<Route element={<MainLayout />}>
 				<Route path="/" element={<Outlet />}>
-					<Route index element={<div>Route /</div>} />
-					<Route path="contacts" element={<div>Route /contacts</div>} />
+					<Route index element={<div>path = '/'</div>} />
+					<Route path="contacts" element={<div>path = '/contacts'</div>} />
 					<Route
 						path="contacts/:contactId"
-						element={<div>Route /contacts/:contactId</div>}
+						element={<div>path = '/contacts/:contactId'</div>}
 					/>
-					<Route path="user" element={<div>Route /user</div>} />
-					<Route path="user/edit" element={<div>Route /user/edit</div>} />
+					<Route path=":user" element={<div>path = '/:user'</div>} />
+					<Route path=":user/edit" element={<div>path = '/:user/edit'</div>} />
 				</Route>
-				<Route path="/login" element={<div>Route /login</div>} />
-				<Route path="/register" element={<div>Route /register</div>} />
+				<Route path="/login" element={<div>path = '/login'</div>} />
+				<Route path="/register" element={<div>path = '/register'</div>} />
 			</Route>
 			<Route path="*" element={<div>Page 404</div>} />
 		</Routes>
---
+++ src/components/layouts/main-layout/main-layout.jsx
@@ -0,0 +1,28 @@
+import { Outlet } from 'react-router-dom';
+import styles from './main-layout.module.scss';
+
+function MainLayout() {
+	return (
+		<div className={styles.layout}>
+			{/* @TODO:  Заменить <header> на Header */}
+			<header
+				style={{
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+					height: 83,
+					backgroundColor: '#EAEAEA',
+					width: '100%',
+				}}
+			>
+				Заглушка Header
+			</header>
+
+			<main className={styles.layout__content}>
+				<Outlet />
+			</main>
+		</div>
+	);
+}
+
+export default MainLayout;
