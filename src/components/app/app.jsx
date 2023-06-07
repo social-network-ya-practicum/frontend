1ce0f61c69efe4e5b4721e271bb932da31df17b8
Aliaksandr Lahunou <mymail15-87y@yandex.ru>
2023-06-07 16:47:04 +0300

    feat: добавил компоненты страниц login-page && register-page

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,5 +1,7 @@
 import { Outlet, Route, Routes } from 'react-router-dom';
 import MainLayout from '../layouts/main-layout/main-layout';
+import LoginPage from '../../pages/login-page/login-page';
+import RergisterPage from '../../pages/register-page/register-page';
 
 function App() {
 	return (
@@ -15,8 +17,8 @@ function App() {
 					<Route path=":user" element={<div>path = '/:user'</div>} />
 					<Route path=":user/edit" element={<div>path = '/:user/edit'</div>} />
 				</Route>
-				<Route path="/login" element={<div>path = '/login'</div>} />
-				<Route path="/register" element={<div>path = '/register'</div>} />
+				<Route path="/login" element={<LoginPage />} />
+				<Route path="/register" element={<RergisterPage />} />
 			</Route>
 			<Route path="*" element={<div>Page 404</div>} />
 		</Routes>
