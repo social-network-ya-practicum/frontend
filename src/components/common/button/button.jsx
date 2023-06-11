3f35d1f1313b406399ef0f0e910c57c4d2257363
Aliaksandr Lahunou <mymail15-87y@yandex.ru>
2023-06-11 09:06:20 +0300

    refactor: исправил варианты button(удалил variant "text")

--- src/components/common/button/button.jsx
+++ src/components/common/button/button.jsx
@@ -4,23 +4,8 @@ import styles from './button.module.scss';
 
 const cn = classNames.bind(styles);
 
-function Button({
-	children,
-	type,
-	variant,
-	width,
-	outlined,
-	active,
-	disabled,
-	mix,
-	onClick,
-}) {
-	const cnButton = cn(
-		`button-${variant}`,
-		{ [`button-${variant}_outlined`]: outlined },
-		{ [`button-${variant}_active`]: active },
-		mix
-	);
+function Button({ children, type, variant, width, disabled, mix, onClick }) {
+	const cnButton = cn(`button-${variant}`, mix);
 
 	return (
 		<button
@@ -40,10 +25,8 @@ export default Button;
 Button.propTypes = {
 	children: PropTypes.node.isRequired,
 	type: PropTypes.oneOf(['button', 'submit']),
-	variant: PropTypes.oneOf(['primary', 'secondary', 'text']),
+	variant: PropTypes.oneOf(['primary', 'secondary']),
 	width: PropTypes.string,
-	outlined: PropTypes.bool,
-	active: PropTypes.bool,
 	disabled: PropTypes.bool,
 	mix: PropTypes.string,
 	onClick: PropTypes.func,
@@ -53,8 +36,6 @@ Button.defaultProps = {
 	type: 'button',
 	variant: 'primary',
 	width: 'max-content',
-	outlined: false,
-	active: false,
 	disabled: false,
 	mix: undefined,
 	onClick: undefined,
