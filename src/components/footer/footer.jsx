2ad418a02b2bb8b82b7adffbbffa04fe435255e7
Iri-Sha <a72999555@gmail.com>
2023-06-20 19:43:26 +0500

    fix(component): fix: Изменение footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import Logo from '../../image/logo.svg';
-import BorderGradient from '../common/border-gradient/border-gradient';
 import styles from './footer.module.scss';
 
 function Footer({ user, mix }) {
   return (
     <footer className={mix}>
       <div className={styles.footer}>
-        <div className={styles.foooter__info}>
-          {user && (
+        <div className={styles.footer__container}>
+          <div className={styles.footer__info}>
             <nav>
               <ul className={styles.footer__menu}>
                 <li>
@@ -18,28 +16,27 @@ function Footer({ user, mix }) {
                     Помощь
                   </NavLink>
                 </li>
-                <li>
-                  <NavLink to="/#" className={styles.footer__text}>
-                    Документы
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink to="/#" className={styles.footer__text}>
-                    Реквизиты компании
-                  </NavLink>
-                </li>
+                {user && (
+                  <>
+                  <li>
+                    <NavLink to="/#" className={styles.footer__text}>
+                      Документы
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/#" className={styles.footer__text}>
+                      Реквизиты компании
+                    </NavLink>
+                  </li>
+                  </>
+                )}
               </ul>
             </nav>
-          )}
+          </div>
+          <p className={styles.footer__text_size_s}>
+            {new Date().getFullYear()} Все права защищены
+          </p>
         </div>
-        <NavLink to="/" className={styles.footer__logo}>
-          <BorderGradient>
-            <img className={styles.footer__img} src={Logo} alt="Логотип" />
-          </BorderGradient>
-        </NavLink>
-        <p className={styles.footer__text_size_s}>
-          {new Date().getFullYear()} Все права защищены
-        </p>
       </div>
     </footer>
   );
