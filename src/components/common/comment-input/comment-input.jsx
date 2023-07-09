7be593d55bd489e4134577d5ccb23e050aa4dc11
Amir Ashizhev <amirashizhev@yandex.ru>
2023-07-09 17:16:38 +0300

    feat/comments: авто высота инпута

--- src/components/common/comment-input/comment-input.jsx
+++ src/components/common/comment-input/comment-input.jsx
@@ -1,18 +1,40 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styles from './comment-input.module.scss';
 import RoundIcon from '../round-icon/round-icon';
 import defaultAvatar from '../../../image/defaultAvatar.svg';
 
 function CommentInput() {
-  // const [isInputActive, setIsInputActive] = useState(false);
+  const [isInputActive, setIsInputActive] = useState(false);
   const [value, setValue] = useState('');
+  const [heightText, setHeightText] = useState('px');
   const onChange = (event) => setValue(event.target.value);
+
+  const textStyle = {
+    height: heightText,
+  };
+
+  useEffect(() => {
+    setHeightText('auto');
+  }, [value]);
+
   const handleChange = (event) => {
-    // setHeightText('auto');
+    setHeightText('auto');
     if (onChange) {
       onChange(event);
     }
   };
+
+  const handleInput = (event) => {
+    const { target } = event;
+    target.style.height = 'auto';
+    target.style.height = `${target.scrollHeight}px`;
+    setHeightText(`${target.scrollHeight}px`);
+  };
+
+  function hanldeActiveInput() {
+    setIsInputActive(true);
+  }
+
   return (
     <div className={styles.commentInput}>
       <form className={styles.commentInput__form}>
@@ -26,20 +48,26 @@ function CommentInput() {
             minLength={1}
             maxLength={2000}
             className={styles.commentInput__textarea}
+            onClick={hanldeActiveInput}
+            onInput={handleInput}
+            style={textStyle}
+            rows={1}
           >
             cc
           </textarea>
         </div>
 
-        <div className={styles.commentInput__btns}>
-          <button type="button" className={styles.commentInput__btnCancel}>
-            Отменить
-          </button>
+        {isInputActive && (
+          <div className={styles.commentInput__btns}>
+            <button type="button" className={styles.commentInput__btnCancel}>
+              Отменить
+            </button>
 
-          <button type="button" className={styles.commentInput__btnSave}>
-            Опубликовать
-          </button>
-        </div>
+            <button type="button" className={styles.commentInput__btnSave}>
+              Опубликовать
+            </button>
+          </div>
+        )}
       </form>
     </div>
   );
